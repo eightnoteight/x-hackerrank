30e0a9ccc484862843cad3d6f2dd64ba56c108a3
eightnoteight <mr.eightnoteight@gmail.com>
2014-07-15 14:03:32 +0530

    stdout only version

--- src/x-hackerrank.py
+++ src/x-hackerrank.py
@@ -30,23 +30,29 @@ parser.add_argument('-o',
 parser.add_argument('-l', 
 					'--lang', 
 					dest='lang', 
-					default='C++',
+					default=2,
+					type=int,
 					help='Output file')
 parser.add_argument('--api', 
 					dest='api', 
-					default='YOURAPIKEY',#TODO: load the api key from a default file
+					default='yourapikeyhere',#TODO: load the api key from a default file
 					help='Output file')
 args = parser.parse_args()
 params = {
 			'source' 	: args.source.read() ,
 			'lang'		: args.lang ,
-			'testcases'	: json.dumps( args.input.read() ) ,
+			'testcases'	: "[" + json.dumps(args.input.read()) + "]" ,
 			'api_key'	: args.api,
-			'wait'		: 'true',
+			'wait'		: 1,
 			'format'	: 0
 		 } 
 resp = requests.post(url, params)
-print resp.json()
+json_output = resp.json()
+stdout = json_output['result']['stdout']
+for x in xrange(0,len(stdout)):
+	print stdout[x]
+
+
 args.output.close()
 args.input.close()
 args.source.close()
